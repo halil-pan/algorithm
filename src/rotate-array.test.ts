08435e6444d1b47210f5953f248410e68c77aa12
STPace <heliapan@outlook.com>
2022-05-04 17:00:45 +0800

    fix: tests rename

--- src/rotate-array.test.ts
+++ src/rotate-array.test.ts
@@ -1,7 +1,7 @@
 import {strict as assert} from 'node:assert';
 import {rotateArray} from './rotate-array';
 
-export function testrotateArray() {
+export function test() {
   assert.deepEqual(rotateArray([1, 2, 3, 4, 5], 3), [3, 4, 5, 1, 2]);
 
   assert.deepEqual(rotateArray([1, 2, 3, 4, 5], 8), [1, 2, 3, 4, 5]);
